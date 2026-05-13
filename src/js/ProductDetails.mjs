c9b01ea64e2733b70cf87fd86bb4b3f78568bf44
misswendiaz <ldiaz@byupathway.edu>
2026-05-13 23:32:21 +0800

    W02 Team Activity: Dynamic Product Detail Page

---
+++ src/js/ProductDetails.mjs
@@ -0,0 +1,63 @@
+
+// Dynamically produces the product detail pages
+import { getLocalStorage, setLocalStorage } from "./utils.mjs";
+
+export default class ProductDetails {
+    constructor(productId, dataSource) {
+        this.productId = productId;
+        this.product = {};
+        this.dataSource = dataSource;
+    }
+
+    // initialize page
+    async init() {
+        this.product = await this.dataSource.findProductById(this.productId);
+
+        console.log(this.product);
+        console.log(this.product.Image);
+
+        this.renderProductDetails();
+
+        // add listener to Add to Cart button
+        document
+            .getElementById("addToCart")
+            .addEventListener("click", this.addProductToCart.bind(this));
+
+    }
+
+    addProductToCart() {
+        let cartItems = getLocalStorage("so-cart") || [];
+        cartItems.push(this.product);
+        setLocalStorage("so-cart", cartItems);
+    }
+
+    renderProductDetails() {
+        document.querySelector(".product-detail h3").textContent =
+            this.product.Brand.Name;
+
+        document.querySelector(".product-detail h2").textContent =
+            this.product.Name;
+
+        document.querySelector(".product-detail img").src =
+            this.product.Image;
+
+        document.querySelector(".product-detail img").alt =
+            this.product.Name;
+
+        document.querySelector(".product-card__price").textContent =
+            `$${this.product.FinalPrice}`;
+
+        document.querySelector(".product__color").textContent =
+            this.product.Colors[0].ColorName;
+
+        document.querySelector(".product__description").textContent =
+            this.product.Description;
+
+        document.querySelector("#addToCart").dataset.id =
+            this.product.Id;
+    }
+}
+
+
+
+
